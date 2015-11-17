63b02243659d74ddd2425b17457bc143ac1de16e
cool-delete <kk520149492@gmail.com>
2015-11-18 04:51:41 +0800

    初始化按钮

--- app/src/main/java/lovejazzie/clock/StopWatchView.java
+++ app/src/main/java/lovejazzie/clock/StopWatchView.java
@@ -2,12 +2,19 @@ package lovejazzie.clock;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 /**
  * Created by Administrator on 2015/11/16.
  */
-public class StopWatchView extends LinearLayout {
+public class StopWatchView extends LinearLayout implements View.OnClickListener {
+    Button btnStart, btnPause, btnResume, btnLap, btnReset;
+    Button[] btns = {btnStart, btnPause, btnResume, btnLap, btnReset};
+    int[] btnid = {R.id.btnSWStart, R.id.btnSWPause, R.id.btnSWResume, R.id.btnSWLap, R.id.btnSWReset};
+
+
     public StopWatchView(Context context) {
         super(context);
     }
@@ -16,4 +23,26 @@ public class StopWatchView extends LinearLayout {
         super(context, attrs);
     }
 
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        initBtn();
+    }
+
+    private void initBtn() {
+        for (int i = 0; i < btnid.length; i++) {
+            btns[i] = (Button) findViewById(btnid[i]);
+            btns[i].setOnClickListener(this);
+        }
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnSWStart:
+
+                break;
+        }
+    }
 }
